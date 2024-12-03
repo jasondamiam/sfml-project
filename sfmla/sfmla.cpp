301f5bd8af3d2755ee1315e1c8c52080d2c676e0
jasondamiam <144017862+jasondamiam@users.noreply.github.com>
2024-12-03 11:19:35 +0100

    movement

--- sfmla/sfmla.cpp
+++ sfmla/sfmla.cpp
@@ -5,7 +5,8 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1000, 600) , "SFML works!");
+
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
 
@@ -17,7 +18,7 @@ int main()
     texture.loadFromFile("./Ribbit.png");
     sf::Sprite sprite;
     sprite.setTexture(texture);
-    sprite.setScale(sf::Vector2f(2, 2));
+    sprite.setScale(2.f, 2.f);
 
     while (window.isOpen())
     {
@@ -27,6 +28,47 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
+
+        float moveSpeed = 0.5f;
+        sf::Vector2f movement(0.f, 0.f);
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            movement.x -= moveSpeed;
+            sprite.setScale(-2.f, 2.f);
+            sprite.setOrigin(sprite.getLocalBounds().width, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            movement.x += moveSpeed;
+            sprite.setScale(2.f, 2.f);
+            sprite.setOrigin(0, 0);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            movement.y -= moveSpeed;
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            movement.y += moveSpeed;
+        }
+
+
+        sf::Vector2u windowSize = window.getSize();
+        sf::FloatRect spriteBounds = sprite.getGlobalBounds();
+
+        sf::Vector2f newPosition = sprite.getPosition() + movement;
+
+        if (newPosition.x < 0) {
+            newPosition.x = 0;
+        }
+        if (newPosition.y < 0) {
+            newPosition.y = 0;
+        }
+        if (newPosition.x + spriteBounds.width > windowSize.x) {
+            newPosition.x = windowSize.x - spriteBounds.width;
+        }
+        if (newPosition.y + spriteBounds.height > windowSize.y) {
+            newPosition.y = windowSize.y - spriteBounds.height;
+        }
+
+        sprite.setPosition(newPosition);
 
         window.clear();
         window.draw(shape);
